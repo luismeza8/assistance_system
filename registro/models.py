c2d85f123e5cb5e8bb59495127c645efce3e061a
luismeza8 <angel841bv@gmail.com>
2024-04-29 09:37:45 -0700

    template de eliminar miembro estilizado

--- registro/models.py
+++ registro/models.py
@@ -10,7 +10,7 @@ class Subsistema(models.Model):
 
 class Mision(models.Model):
     nombre = models.CharField(max_length=100)
-    lider = models.ForeignKey('Miembro', on_delete=models.CASCADE, related_name='lider_de')
+    lider = models.ForeignKey('Miembro', on_delete=models.CASCADE, related_name='lider_de', null=True, blank=True)
     fecha_inicio = models.DateField()
     fecha_finalizacion = models.DateField()
 
