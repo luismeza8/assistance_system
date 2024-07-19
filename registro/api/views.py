fe86c9f17a29a1f7eb32aff3b64c7b58ce8efaab
luismeza8 <angel841bv@gmail.com>
2024-07-18 19:53:58 -0700

    hours worked per register added to the register table

--- registro/api/views.py
+++ registro/api/views.py
@@ -10,7 +10,7 @@ def agregar_registro(request):
     member = Miembro.objects.get(pk=member_id)
     last_register = member.register_set.last()
 
-    if last_register.check_in_time == None or last_register.check_out_time != None:
+    if not last_register or last_register.check_in_time == None or last_register.check_out_time != None:
         serializer = RegistroSerializer(data=request.data)
     else:
         serializer = RegistroSerializer(instance=last_register, data=request.data)
--- registro/models.py
+++ registro/models.py
@@ -11,6 +11,32 @@ class Register(models.Model):
     check_out_time = models.TimeField(null=True)
 
 
+    def _get_minutes_to_seconds(self, minutes):
+        return minutes * 60
+    
+
+    def _get_hours_to_seconds(self, hours):
+        return hours * 3600
+
+
+    @property
+    def hours_worked(self):
+        if self.check_in_time and self.check_out_time:
+            check_in_seconds = self.check_in_time.hour * 3600 + self.check_in_time.minute * 60 + self.check_in_time.second
+            check_out_seconds = self.check_out_time.hour * 3600 + self.check_out_time.minute * 60 + self.check_out_time.second
+
+            result = check_out_seconds - check_in_seconds
+
+            hours = result / 3600
+            minutes = (hours % 1) * 60
+
+            hours = f'0{int(hours)}' if hours < 10 else int(hours) 
+            minutes = f'0{int(minutes)}' if minutes < 10 else int(minutes)
+
+            return f'{hours}:{minutes}'
+        return '--.--'
+
+
     def save(self, *args, **kwargs):
         if self.check_in_time == None:
             self.check_in_time = timezone.now().time()
@@ -21,7 +47,7 @@ class Register(models.Model):
 
 
     def __str__(self):
-        return f'{self.member}'
+        return f'{self.member} {self.check_in_time}-{self.check_out_time}'
 
 
     # https://pynative.com/python-datetime-format-strftime/
