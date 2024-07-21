d71d86a03de734ea379bcf4de33c9f13d4cf947c
luismeza8 <angel841bv@gmail.com>
2024-07-20 23:58:10 -0700

    the register chart now loads from a external js file

--- static/js/register/register.js
+++ static/js/register/register.js
@@ -1,8 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const value = JSON.parse(document.getElementById('hours').textContent);
-  console.log(value)
+  (() => {
+    const ctx = document.getElementById('register_chart');
+    const value = JSON.parse(document.getElementById('hours').textContent);
 
-  htmx.onLoad(function(){
-    console.log('yeap')
-  })
-})
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
+        datasets: [{
+          label: 'Horas trabajadas cada dia',
+          backgroundColor: '#1e1d4f',
+          data: value,
+        }]
+      },
+      options: {
+        aspectRatio: 5,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  })();
